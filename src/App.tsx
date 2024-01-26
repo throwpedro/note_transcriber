8632c2397d6cfd5b729400453fc35dd511584f74
thor <thor@colourbox.com>
2024-01-26 21:51:29 +0100

    shadcn textarea lol.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Textarea } from "@/components/ui/textarea"
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <Textarea />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
